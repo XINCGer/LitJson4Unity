f249b67bad0a2e2584e2a927257d3c2529f85880
XinCGer <1790617178@qq.com>
2020-03-23 19:52:11 +0800

    添加测试工程代码

---
+++ Assets/Editor/Converter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using LitJson;
+using System.IO;
+
+public class Converter 
+{
+    [MenuItem("ColaFramework/序列化为Json")]
+    public static void Trans2Json()
+    {
+        var asset = AssetDatabase.LoadAssetAtPath<TestScriptableObj>("Assets/TestScriptableObj.asset");
+        var jsonContent = JsonMapper.ToJson(asset);
+        using(var stream = new StreamWriter("Assets/TestScriptableObj.json"))
+        {
+            stream.Write(jsonContent);
+        }
+        AssetDatabase.Refresh();
+    }
+}
---
+++ Assets/Scripts/TestScriptableObj.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LitJson.Extensions;
+
+[CreateAssetMenu(fileName = "TestScriptableObj",menuName = "ColaFramework/TestScriptableObj")]
+public class TestScriptableObj:ScriptableObject
+{
+    public int num1;
+
+    public float num2;
+
+    public Vector2 v2;
+
+    public Vector3 v3;
+
+    public Quaternion quaternion;
+
+    public Color color1;
+
+    public Color32 color2;
+
+    public Bounds bounds;
+
+    public Rect rect;
+
+    public AnimationCurve animationCurve;
+
+    [JsonIgnore]
+    public string SerializeField;
+}
