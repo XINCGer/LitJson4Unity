a1f6e121125d879b08ffb17a8c48fc961e4d1f69
马三小伙儿 <1790617178@qq.com>
2020-03-29 19:55:11 +0800

    更新测试例子

--- Assets/Editor/Converter.cs
+++ Assets/Editor/Converter.cs
@@ -7,15 +7,32 @@ using System.IO;
 
 public class Converter 
 {
+    private static readonly string JsonPath = "Assets/TrasnferData/TestScriptableObj.json";
+    private static readonly string ScriptableObjectPath = "Assets/TestScriptableObj.asset";
+    private static readonly string TransScritptableObjectPath = "Assets/TrasnferData/TestScriptableObj.asset";
+
     [MenuItem("ColaFramework/序列化为Json")]
     public static void Trans2Json()
     {
-        var asset = AssetDatabase.LoadAssetAtPath<TestScriptableObj>("Assets/TestScriptableObj.asset");
+        var asset = AssetDatabase.LoadAssetAtPath<TestScriptableObj>(ScriptableObjectPath);
         var jsonContent = JsonMapper.ToJson(asset);
-        using(var stream = new StreamWriter("Assets/TestScriptableObj.json"))
+        using(var stream = new StreamWriter(JsonPath))
         {
             stream.Write(jsonContent);
         }
         AssetDatabase.Refresh();
+    }
+
+    [MenuItem("ColaFramework/反序列化为ScriptableObject")]
+    public static void Trans2ScriptableObject()
+    {
+        if (!File.Exists(JsonPath)) return;
+        using(var stream = new StreamReader(JsonPath))
+        {
+            var jsonStr = stream.ReadToEnd();
+            var striptableObj = JsonMapper.ToObject<TestScriptableObj>(jsonStr);
+            AssetDatabase.CreateAsset(striptableObj, TransScritptableObjectPath);
+            AssetDatabase.Refresh();
+        }
     }
 }
